d4e955f5637b6456889e8392685bd2b8609abc6c
Nguyễn Lộc <loc3120@gmail.com>
2021-11-06 22:37:59 +0700

    Thêm đường dẫn lưu file

--- src/main/java/com/spring/backend/common/Constants.java
+++ src/main/java/com/spring/backend/common/Constants.java
@@ -3,4 +3,5 @@ package com.spring.backend.common;
 public class Constants {
 	public static final String API_URL = "api/hoctructuyen/";
 	public static final String CROSS_ORIGIN = "http://localhost:3000";
+	public static final String PATH_SAVE_FILE = "E:\\ProjectWeb\\";
 }
